9fc280d2c7867885af956fa8a1f73af159d7a08f
Teresa <tenjerim@gmail.com>
2023-04-13 16:07:24 +0300

    product model

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,2 +1,11 @@
 class ProductCategory < ApplicationRecord
+    belongs_to :product
+  belongs_to :category
+
+  validates :product_id, {presence: true }
+  validates :category_id, {presence: true }
+
+  def self.filter_by_product(product_id) 
+    self.where(product_id: product_id).all
+  end
 end
