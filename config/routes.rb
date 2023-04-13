aba2e87d11717677d641e6d986fd23332063883d
GladysKigen <gladys.kigen@student.moringa.com>
2023-04-13 23:53:18 +0300

    created the Orders resource

--- config/routes.rb
+++ config/routes.rb
@@ -23,3 +23,28 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+Rails.application.routes.draw do
+
+  resources :orders
+  resources :admin_users
+
+
+  resources :order_items
+
+
+
+  resources :product_categories
+  resources :products
+
+
+  resources :admin_products
+  resources :payments
+  resources :categories
+
+  resources :admins
+  resources :users
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
