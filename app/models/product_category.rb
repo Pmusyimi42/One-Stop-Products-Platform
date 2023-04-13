c0fe4ad8a84467abd67348f1f1e87eb0c7518d42
Teresa <tenjerim@gmail.com>
2023-04-13 17:59:44 +0300

    associations

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,6 +1,6 @@
 class ProductCategory < ApplicationRecord
     belongs_to :product
-  belongs_to :category
+    belongs_to :category
 
 #   validates :product_id, {presence: true }
 #   validates :category_id, {presence: true }
