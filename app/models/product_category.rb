2034bb4686cd2220c9fddbf06168682855bdf21f
Teresa <tenjerim@gmail.com>
2023-04-15 19:50:15 +0300

    product actions and validations

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,10 +2,10 @@ class ProductCategory < ApplicationRecord
     belongs_to :product
     belongs_to :category
 
-#   validates :product_id, {presence: true }
-#   validates :category_id, {presence: true }
+  validates :product_id, {presence: true }
+  validates :category_id, {presence: true }
 
-#   def self.filter_by_product(product_id) 
-#     self.where(product_id: product_id).all
+  def self.filter_by_product(product_id) 
+    self.where(product_id: product_id).all
   end
 end
