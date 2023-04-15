ea72968c3c05bbe73e2be2e20b243a565b869c04
Teresa <tenjerim@gmail.com>
2023-04-15 21:17:38 +0300

    product actions and validations

--- db/migrate/20230413114922_create_products.rb
+++ db/migrate/20230413114922_create_products.rb
@@ -2,6 +2,9 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
 
+      t.string :title
+      t.text :description
+      
       t.timestamps
     end
   end
