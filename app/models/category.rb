5dc72efc418f4f05d9a07005916e4ad9807dcf8d
patrick kisyula <pmusyimi42@gmail.com>
2023-04-15 22:40:08 +0300

    added validations to admin_product.rb, category.rb, and payment.rb, also populated seed file

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
     has_many :product_categories, dependent: :destroy
     has_many :products, through: :product_categories, dependent: :destroy
+
+    validates :name, presence: true, length: { maximum: 255 }
+
 end
