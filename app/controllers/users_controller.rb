723ec466c65ac6d21af6dc8be9fb2dbc421fda12
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-04-15 14:21:34 +0300

    user-actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,40 @@
 class UsersController < ApplicationController
 
     def index
-        
-    end
-
-    def show
-        
-    end
-
-    def destroy
-
-    end
-    def create
-        
-    end
+        render json: User.all
+      end
+  
+      def show
+        user = find_users
+        render json: user, status: :ok
+      end
+  
+      def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+      end
+  
+      def update
+        user = find_users
+        user.update!(user_params)
+        render json: user, except: [:created_at, :updated_at], status: :created
+      end
+  
+      def destroy
+        user = find_users
+        user.destroy
+        head :no_content
+      end
+  
+      private
+  
+      def find_users
+        User.find(params[:id])
+      end
+  
+      def user_params
+        params.permit(:id, :applicant_id, :course_id, :documents, :status)
+     end
+  
+     
 end
