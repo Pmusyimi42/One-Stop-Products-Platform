d3a352b16963a761fd4beed6ae6d4770ec1e0ec2
Teresa <tenjerim@gmail.com>
2023-04-16 11:03:17 +0300

    product actions and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_13_205010) do
+ActiveRecord::Schema.define(version: 2023_04_16_080059) do
 
   create_table "admin_products", force: :cascade do |t|
     t.integer "admin_id", null: false
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2023_04_13_205010) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "imageUrl"
   end
 
   create_table "users", force: :cascade do |t|
