d6e973bd8343ce88392f15f141f6347dd5846df5
patrick kisyula <pmusyimi42@gmail.com>
2023-04-21 15:22:30 +0300

    added the post user component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SingleProduct from './components/SingleProduct';
+import AddNewUser from './components/AddNewUser';
 
 function App() {
   return (
     <div className="App">
 
       <SingleProduct/>
+      <AddNewUser/>
 
     </div>
   );
