cefa4e88453c711eebcbac165ddb37b71924c5ec
patrick kisyula <pmusyimi42@gmail.com>
2023-04-23 17:12:06 +0300

    replaced static data with dynamic data

--- client/src/components/Orders.jsx
+++ client/src/components/Orders.jsx
@@ -1,24 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Orders.css';
 
 export default function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [statusFilter, setStatusFilter] = useState('');
+  const [dateFilter, setDateFilter] = useState('');
+
+  useEffect(() => {
+    let url = '/orders';
+    if (statusFilter) {
+      url += `?status=${statusFilter}`;
+    }
+    if (dateFilter) {
+      url += `${statusFilter ? '&' : '?'}date=${dateFilter}`;
+    }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setOrders(data));
+  }, [statusFilter, dateFilter]);
+
+  const renderOrdersTable = () => {
+    return orders.map((order) => (
+      <tr key={order.id}>
+        <td>{order.created_at.slice(0, 10)}</td>
+        <td>{order.id}</td>
+        <td>{order.payment.status}</td>
+        <td>${order.payment.amount}</td>
+        {/* <td>{order.itemsSold}</td> */}
+      </tr>
+    ));
+  };
+
+  const handleStatusFilterChange = (e) => {
+    setStatusFilter(e.target.value);
+  };
+
+  const handleDateFilterChange = (e) => {
+    setDateFilter(e.target.value);
+  };
+
+  const netSales = orders.reduce((acc, order) => acc + Number(order.payment.amount), 0).toFixed(2);
+
+
   return (
     <div className="orders-container">
-        <h1 className='orders-title'>Orders</h1>
+      <h1 className="orders-title">Orders</h1>
       <div className="orders-header">
         <div className="orders-header-item">
           <h1>Orders</h1>
-          <h1>69</h1>
+          <h1>{orders.length}</h1>
         </div>
         <div className="orders-header-item">
-          <h1>Net Sales</h1> 
-          <h1>$30,305</h1>
+          <h1>Net Sales</h1>
+          <h1>
+            ${netSales}
+          </h1>
         </div>
       </div>
       <div className="orders-search">
         <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Search By Status" aria-label="Search"/>
-          <input className="form-control me-2" type="date" placeholder="Transaction Date" aria-label="Transaction Date"/>
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search By Status"
+            aria-label="Search"
+            value={statusFilter}
+            onChange={handleStatusFilterChange}
+          />
+          <input
+            className="form-control me-2"
+            type="date"
+            placeholder="Transaction Date"
+            aria-label="Transaction Date"
+            value={dateFilter}
+            onChange={handleDateFilterChange}
+          />
         </form>
       </div>
       <div className="orders-table">
@@ -29,25 +85,10 @@ export default function Orders() {
               <th>Order</th>
               <th>Status</th>
               <th>Net Sales</th>
-              <th>Items Sold</th>
+              {/* <th>Items Sold</th> */}
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>22/4/2023</td>
-              <td>5</td>
-              <td>Successful</td>
-              <td>$54</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>2/4/2023</td>
-              <td>6</td>
-              <td>Pending</td>
-              <td>$34</td>
-              <td>4</td> 
-            </tr>
-          </tbody>
+          <tbody>{renderOrdersTable()}</tbody>
         </table>
       </div>
     </div>
