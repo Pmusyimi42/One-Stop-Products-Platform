6ce3991b7683dc32ed09be512039c1886863b827
patrick kisyula <pmusyimi42@gmail.com>
2023-04-26 23:58:27 +0300

    Revert "added footer"
    This reverts commit 6c8b2845796d3f59b34e28801c3d131fdc742d07.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home';
-
+// import UserDetails from './components/UserDetails';
 import SingleProduct from './components/SingleProduct';
 import Navbar from "./components/Navbar";
 import Products from "./pages/products";
@@ -11,7 +11,9 @@ import About from "./pages/About";
 import SignUpForm from "./components/SignUpForm";
 
 import AddProducts from './components/AddProduct';
-
+// import Preview from './components/Preview';
+// import AddProductForm from './components/AddProductForm';
+// import UserDetails from './components/UserDetails';
 import ProductList from './components/ProductList';
 import Dashboard from './components/Dashboard';
 import AddNewUser from './components/AddNewUser';
@@ -19,9 +21,6 @@ import EditProduct from "./components/EditProduct";
 import UserDetials from "./components/UserDetails";
 import AdminSection from "./components/AdminSection";
 
-import Footer from "./components/layout/Footer";
-
-import LoginForm from "./components/LoginForm";
 
 
 
@@ -53,7 +52,6 @@ export default function App() {
 
           <Route path='/dashboard' element={<Dashboard />} />
         </Switch>
-        <Footer/>
       </Router>
     </div>
 
