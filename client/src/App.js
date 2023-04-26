6c8b2845796d3f59b34e28801c3d131fdc742d07
patrick kisyula <pmusyimi42@gmail.com>
2023-04-26 21:10:59 +0300

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home';
-// import UserDetails from './components/UserDetails';
+
 import SingleProduct from './components/SingleProduct';
 import Navbar from "./components/Navbar";
 import Products from "./pages/products";
@@ -11,17 +11,15 @@ import About from "./pages/About";
 import SignUpForm from "./components/SignUpForm";
 
 import AddProducts from './components/AddProduct';
-// import Preview from './components/Preview';
-// import AddProductForm from './components/AddProductForm';
-// import UserDetails from './components/UserDetails';
+
 import ProductList from './components/ProductList';
 import Dashboard from './components/Dashboard';
 import AddNewUser from './components/AddNewUser';
 import EditProduct from "./components/EditProduct";
 import UserDetials from "./components/UserDetails";
 import AdminSection from "./components/AdminSection";
-// import EditProduct from "./components/EditProduct";
-// import UserDetials from "./components/UserDetials";
+import Footer from "./components/layout/Footer";
+
 
 
 
@@ -51,6 +49,7 @@ export default function App() {
 
           <Route path='/dashboard' element={<Dashboard />} />
         </Switch>
+        <Footer/>
       </Router>
     </div>
 
