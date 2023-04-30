a755518d0918771c38de53a3ba06a3b84b6b18f3
Kellymugambi <kellymgambi@.com>
2023-04-30 15:38:43 +0300

    modified files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 
 import SingleProduct from './components/SingleProduct';
 import Navbar from "./components/Navbar";
 import Products from "./pages/products";
 import About from "./pages/About";
+import Cart from "./components/Cart";
+
 
 import SignUpForm from "./components/SignUpForm";
 
@@ -18,44 +20,89 @@ import AddNewUser from './components/AddNewUser';
 import EditProduct from "./components/EditProduct";
 import UserDetials from "./components/UserDetails";
 import AdminSection from "./components/AdminSection";
-
-import Footer from "./components/layout/Footer";
-
 import LoginForm from "./components/LoginForm";
+import Footer from "./components/layout/Footer";
 
 
 
 
 export default function App() {
+
+  const [cart, setCart] = useState([])
+
+    useEffect(() =>{
+        fetch("/users/2")
+         .then((response) => response.json())
+         .then((data) => {
+            setCart(data.cart)
+         })
+        //  console.log(refresher)
+    },[]);
+
+function removeFromCart(id){
+    setCart(cart.filter((crt)=>{
+        return crt.id !== id 
+    }))
+}
+
+function addToCart(cart_item){
+  if (cart.find((item)=> item.id === cart_item.id)){
+    setCart(cart.map((cartItem)=>{ 
+      if (cartItem.id === cart_item.id){
+        return cart_item
+      }else{
+        return cartItem
+      }
+    }))
+  }else{
+    setCart([...cart, cart_item])
+  }
+}
+ 
+function changeQuantity(qty, item_id){
+    fetch(`/cart_items/${item_id}`,{
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          quantity: qty,
+        }),
+       })
+       .then((res) => res.json())
+       .then((data) => {
+          setCart(cart.map((crt) =>{
+             if (crt.id === item_id) {
+                return {...crt, quantity: data.quantity}
+             }else{
+                return crt
+             }
+          }))
+       })
+}
+
+
+
   return (
 
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-
-
-          <Route path="/userdetails" component={UserDetials} />
-           <Route path='/add_products' component={AddProducts} />
-           <Route path='/editproduct' component={EditProduct} />
-          <Route path='/products_list' component={ProductList} />
-          <Route path='/adminsection' component={AdminSection} />
-          <Route path='/signup' component={SignUpForm} />
-          <Route path='/login' component={LoginForm} />
-
-          <Route path="/products" component={Products} />
-          <Route path="/about" component={About} />
-
-         
-
-          <Route path="/product_categories/:id" component={SingleProduct} />
-
+        <Routes>
+          <Route path="/" exact element={<Home n={cart.length} addToCart={addToCart}/>} />
+          <Route path="/userdetails" element={<UserDetials/>} />
+          <Route path='/add_products' element={<AddProducts/>} />
+          <Route path='/editproduct' element={<EditProduct/>} />
+          <Route path='/products_list' element={<ProductList/>} />
+          <Route path='/adminsection' element={<AdminSection/>} />
+          <Route path='/signup' element={<SignUpForm/>} />
+          <Route path='/cart' element={<Cart cart={cart} setCart={setCart} changeQuantity={changeQuantity} removeFromCart={removeFromCart}/>} />
+          <Route path="/products" element={<Products n={cart.length} addToCart={addToCart}/>} />
+          <Route path="/about" element={<About/>} /> 
+          <Route path="/product_categories/:id" element={<SingleProduct addToCart={addToCart}/>} />
           <Route path='/dashboard' element={<Dashboard />} />
-        </Switch>
-        <Footer/>
+        </Routes>
       </Router>
     </div>
 
   );
 }
+  
